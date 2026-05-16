6a19af5c2ae56ebac361eda2c13a72dd0bf90693
Prince <princemungra1340@gmail.com>
2026-05-16 11:31:22 +0530

    update page

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 
